88208f8996c450f58efdbfdda6b45cea5ab14440
LayneHansen <layne.d.hansen@gmail.com>
2020-12-18 21:25:37 -0700

    fixed image link in card

--- book_search/src/components/TopCard.jsx
+++ book_search/src/components/TopCard.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
+import { Card, Body, Text } from 'react-bootstrap';
+import { CustomPlaceholder } from 'react-placeholder-image';
 
 const TopCard = () => {
     return (
-        <h1>Hello from Top Card</h1>
+        <>
+        <Card>
+            <CustomPlaceholder width={1100} height={200} />
+            {/* <Card.Img variant="top" src={placeholder} /> */}
+            <Card.Body>
+            <Card.Text>
+                Some quick example text to build on the card title and make up the bulk
+                of the card's content.
+            </Card.Text>
+            </Card.Body>
+        </Card>
+        </>
     )
 }
 
