8089ddcb01e4a5a5b131133c81d6a30ccf6f5919
LayneHansen <layne.d.hansen@gmail.com>
2020-12-23 16:25:28 -0700

    added controllers folder and file, began mongoose schema

--- book_search/src/App.js
+++ book_search/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import GlobalProvider from './utils/GlobalContext';
 import Homepage from "./pages/Homepage";
-import SearchPage from "./pages/SearchPage";
+import SavePage from "./pages/SavePage";
 import TopCard from './components/TopCard';
 import './App.css';
 import { Button, Card, Container, Jumbotron, Row, Col, ListGroup } from 'react-bootstrap';
@@ -12,11 +12,10 @@ function App() {
   return (
     <Router>
       <GlobalProvider>
-        {/* <TopCard /> */}
         <div className="App">
           <Switch>
             <Route exact path="/" component={Homepage} />
-            <Route exact path="/search" component={SearchPage} />
+            <Route exact path="/search" component={SavePage} />
             {/* <Route exact path="/todos/new" component={AddTodos} /> */}
           </Switch>
         </div>
